86536b59248c40d544ea33f502d5747a5bb7a737
Isaias Siqueira <isaias.siqueiraa@gmail.com>
2023-06-16 03:05:13 -0300

    Projeto Finalizado

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #black;
+    background-color: #e0e0e7;
+    color: #1c191c;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    fontsize: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #1c191c;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
